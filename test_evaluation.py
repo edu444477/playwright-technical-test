bc096f1b38572bcbd085914b22f22e6f1334388a
edu444477 <eduardoorellanapomares@gmail.com>
2024-07-17 15:06:07 +0200

    Added checkout and logout tests

--- test_evaluation.py
+++ test_evaluation.py
@@ -1,8 +1,6 @@
-# import time
 from playwright.sync_api import expect
 
 # Use pytest test_evaluation.py to run the test
-# Uncomment sleeps and time import to debug
 
 
 def test_saucedemo_page(page):
@@ -13,7 +11,6 @@ def test_saucedemo_page(page):
     page.locator('//input[@id="login-button"]').click()
     # Checking URL to ensure login redirects to the correct page.
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
-    # time.sleep(2)
     # 2 - Add the Sauce Labs Backpack product to the cart
     add_backpack_button = page.locator('//button[@id="add-to-cart-sauce-labs-backpack"]')
     add_backpack_button.click()
@@ -24,20 +21,51 @@ def test_saucedemo_page(page):
     expect(add_backpack_button).not_to_be_visible()
     # Extra check to verify correct cart behavior after adding the product
     expect(page.locator('//span[@class="shopping_cart_badge"]')).to_be_visible()
-    # time.sleep(2)
     # 4 - Add the product “Sauce Labs Bike Light” and “Sauce Labs Fleece Jacket” to the cart
     page.locator('//button[@id="add-to-cart-sauce-labs-bike-light"]').click()
     page.locator('//button[@id="add-to-cart-sauce-labs-fleece-jacket"]').click()
-    # time.sleep(2)
     # 5 - Delete the product “Sauce Labs Backpack” from the cart
     remove_backpack_button.click()
     # 6 - Verify that the button text changes from "Remove" to "Add to cart"
     expect(add_backpack_button).to_have_text("Add to cart")
     # Extra check to verify that the "Remove" button is not visible
     expect(remove_backpack_button).not_to_be_visible()
-    # time.sleep(2)
     # 7 - Click on the cart
     page.locator('//a[@data-test="shopping-cart-link"]').click()
-    # Extra check to verify that the page is as expected
-    page.title = "Your Cart"
-    # time.sleep(5)
+    # 8 - Verify that the url is: https://www.saucedemo.com/cart.html
+    expect(page).to_have_url("https://www.saucedemo.com/cart.html")
+    # 9 - Click on the "Checkout" button.
+    page.locator('//button[@id="checkout"]').click()
+    # 10 - Click on the "Continue" button
+    page.locator('//input[@id="continue"]').click()
+    # 11 - Verify that an error message appears with the text First name is required
+    expect(page.locator('//h3[@data-test="error"]')).to_have_text("Error: First Name is required")
+    # Extra check to verify that the error div is visible
+    expect(page.locator('//div[@class="error-message-container error"]')).to_be_visible()
+    # 12 - Fill out the entire form with your personal information
+    page.locator('//input[@id="first-name"]').fill("Eduardo")
+    page.locator('//input[@id="last-name"]').fill("Orellana")
+    page.locator('//input[@id="postal-code"]').fill("29660")
+    # 13 - Click on the "Continue" button
+    page.locator('//input[@id="continue"]').click()
+    # 14 - Verify that the "Total" is the sum of "Item total" + "Tax"
+    item_total_text = page.locator('div[data-test="subtotal-label"]').text_content()
+    tax_text = page.locator('div[data-test="tax-label"]').text_content()
+    total_text = page.locator('div[data-test="total-label"]').text_content()
+    item_total = float(item_total_text.split('$')[1])
+    tax = float(tax_text.split('$')[1])
+    total = float(total_text.split('$')[1])
+    assert round(item_total + tax, 2) == total, f"Expected total to be {round(item_total + tax, 2)}, but got {total}"
+    # 15 - Click on the "Finish" button
+    page.locator('//button[@id="finish"]').click()
+    # 16 - Verify that the message "Thank you for your order!" appears.
+    expect(page.locator('//h2[@data-test="complete-header"]')).to_have_text("Thank you for your order!")
+    # 17 - Click on the "Back Home" button
+    page.locator('//button[@id="back-to-products"]').click()
+    # 18 - Click on the dropdown menu
+    page.locator('//button[@id="react-burger-menu-btn"]').click()
+    # 19 - Click on "Logout"
+    page.locator('//a[@id="logout_sidebar_link"]').click()
+    # 20 - Verify that the "Username" and "Password" fields are visible
+    expect(page.locator('//input[@id="user-name"]')).to_be_visible()
+    expect(page.locator('//input[@id="password"]')).to_be_visible()
